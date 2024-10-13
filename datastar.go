b7ad865516d7607ab2160b4f4501f90f0c8d25f3
BlinkingLight <into@ituoga.lt>
2024-10-13 21:01:04 +0300

    datastar unmarshal readerfromrequest

--- datastar.go
+++ datastar.go
@@ -1,12 +1,26 @@
 package toolbox
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/valyala/bytebufferpool"
 )
+
+func ReaderFromRequest(r *http.Request) (*bytes.Buffer, error) {
+	if r.Body == nil {
+		return nil, fmt.Errorf("request body is nil")
+	}
+	buf := bytes.NewBuffer(nil)
+	if _, err := buf.ReadFrom(r.Body); err != nil {
+		return nil, fmt.Errorf("failed to read body: %w", err)
+	}
+
+	return buf, nil
+}
 
 func BodyUnmarshal(r io.Reader, store any) error {
 	buf := bytebufferpool.Get()
