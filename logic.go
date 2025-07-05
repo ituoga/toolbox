1008837eadb28057f9557f05ad2fd4655a9066ac
M <mz@9g.lt>
2025-07-05 11:16:07 +0300

    logic

---
+++ logic.go
@@ -0,0 +1,7 @@
+package toolbox
+
+import "github.com/delaneyj/toolbelt"
+
+var Throttle = toolbelt.Throttle
+var Debounce = toolbelt.Debounce
+var CallNTimesWithDelay = toolbelt.CallNTimesWithDelay
