c36efc3a0e9eee9afaaffd4725157aab517cdc13
M <mz@9g.lt>
2025-07-17 09:01:18 +0300

    strings

--- strings.go
+++ strings.go
@@ -2,17 +2,57 @@ package toolbox
 
 import "github.com/delaneyj/toolbelt"
 
-var Pascal = toolbelt.Pascal
-var Camel = toolbelt.Camel
-var Snake = toolbelt.Snake
-var ScreamingSnake = toolbelt.ScreamingSnake
-var Kebab = toolbelt.Kebab
-var Upper = toolbelt.Upper
-var Lower = toolbelt.Lower
+func Pascal(s string) string {
+	return toolbelt.Pascal(s)
+}
 
-type CasedString = toolbelt.CasedString
+func Camel(s string) string {
+	return toolbelt.Camel(s)
+}
 
-var ToCasedString = toolbelt.ToCasedString
+func Snake(s string) string {
+	return toolbelt.Snake(s)
+}
+
+func ScreamingSnake(s string) string {
+	return toolbelt.ScreamingSnake(s)
+}
+
+func Kebab(s string) string {
+	return toolbelt.Kebab(s)
+}
+
+func Upper(s string) string {
+	return toolbelt.Upper(s)
+}
+
+func Lower(s string) string {
+	return toolbelt.Lower(s)
+}
+
+type CasedString struct {
+	Original       string
+	Pascal         string
+	Camel          string
+	Snake          string
+	ScreamingSnake string
+	Kebab          string
+	Upper          string
+	Lower          string
+}
+
+func ToCasedString(s string) CasedString {
+	return CasedString{
+		Original:       s,
+		Pascal:         Pascal(s),
+		Camel:          Camel(s),
+		Snake:          Snake(s),
+		ScreamingSnake: ScreamingSnake(s),
+		Kebab:          Kebab(s),
+		Upper:          Upper(s),
+		Lower:          Lower(s),
+	}
+}
 
 type CasedFn func(string) string
 
