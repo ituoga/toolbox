fc51a7fb2599aff4069d0d9671476cd61511d9d2
M <mz@9g.lt>
2025-08-09 18:21:24 +0300

    get default

--- env/env.go
+++ env/env.go
@@ -22,3 +22,10 @@ func GetWithError(k string) (*sconv.ConverterWithoutErrors, error) {
 	}
 	return nil, ErrNotExist
 }
+
+func GetDefault(key string, fallback string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return fallback
+}
