df83b55f4f92b73834907004967f567668515695
M <mz@9g.lt>
2025-08-22 13:29:39 +0300

    errorgroup with limit

---
+++ semerg/semerg.go
@@ -0,0 +1,38 @@
+package semerg
+
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/sync/semaphore"
+)
+
+type ErrorGroup struct {
+	ctx context.Context
+	eg  *errgroup.Group
+	sem *semaphore.Weighted
+}
+
+func NewMaxSharedCtx(ctx context.Context, max int) *ErrorGroup {
+	eg, _ := errgroup.WithContext(ctx)
+	sem := semaphore.NewWeighted(int64(max))
+	return &ErrorGroup{
+		ctx: ctx,
+		eg:  eg,
+		sem: sem,
+	}
+}
+
+func (g *ErrorGroup) Go(f func() error) {
+	g.eg.Go(func() error {
+		if err := g.sem.Acquire(g.ctx, 1); err != nil {
+			return err
+		}
+		defer g.sem.Release(1)
+		return f()
+	})
+}
+
+func (g *ErrorGroup) Wait() error {
+	return g.eg.Wait()
+}
